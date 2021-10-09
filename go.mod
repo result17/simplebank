9908584413658ae2394e4e82178571db844313f8
chenrongfeng <chenrongfeng@yolonet.io>
2021-10-09 13:05:45 +0800

    complete jwt_marker

--- go.mod
+++ go.mod
@@ -7,13 +7,14 @@ require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/validator/v10 v10.9.0
 	github.com/golang/mock v1.6.0
-	github.com/google/uuid v1.3.0
+	github.com/google/uuid v1.1.2
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lib/pq v1.10.3
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/spf13/viper v1.9.0
 	github.com/stretchr/testify v1.7.0
+	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	golang.org/x/sys v0.0.0-20211004093028-2c5d950f24ef // indirect
 	golang.org/x/text v0.3.7 // indirect
--- token/payload.go
+++ token/payload.go
@@ -6,16 +6,20 @@ import (
 	"time"
 )
 
-var ErrExpiredToken = errors.New("token has expired")
+// Different types of error returned by the VerifyToken function
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
 
 type Payload struct {
-	ID		  uuid.UUID `json:"id"`
+	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	IssuedAt  time.Time `json:"issued_at"`
 	ExpiredAt time.Time `json:"expired-at"`
 }
 
-func NewPayload(username string, duration time.Duration) (*Payload, error)  {
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
 	tokenID, err := uuid.NewRandom()
 	if err != nil {
 		return nil, err
